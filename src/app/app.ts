af7a9642251a2b8a00a585518058696201766c70
Mariame Boughamir <boumariame6@gmail.com>
2026-02-24 22:44:42 +0000

    initial commit:Movie App With TMDB API

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,36 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, inject, signal , OnInit } from '@angular/core';
+import { MovieCard } from './movie-card/movie-card';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MovieCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('movie-app');
+
+  private http = inject(HttpClient);
+  private apiKey = 'ee266460f5057694442159c23d2595cb';
+
+  movies = signal<any[]>([]);
+
+  ngOnInit(){
+    this.fetchMovies();
+  }
+  fetchMovies() {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${this.apiKey}`;
+    
+    this.http.get<any>(url).subscribe((data) => {
+      const formattedMovies = data.results.map((m: any) => ({
+        id: m.id,
+        title: m.title,
+        rating: m.vote_average,
+        img: 'https://image.tmdb.org/t/p/w500' + m.poster_path 
+      }));
+      
+      this.movies.set(formattedMovies); 
+    });
+  }
 }
+
---
+++ src/app/movie-card/movie-card.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-card',
+  imports: [],
+  templateUrl: './movie-card.html',
+  styleUrl: './movie-card.css',
+})
+export class MovieCard {
+  @Input() title: string = ''
+  @Input() imgUrl: string= ''
+  @Input() rating:number=0
+}
